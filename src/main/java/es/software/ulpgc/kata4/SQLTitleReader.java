41ffb96c112570bfadf464e47d49860128cff9a8
kevinjfa <kevin.fa7@outlook.com>
2025-01-10 13:22:58 +0000

    class SQLTitleReader finished

--- src/main/java/es/software/ulpgc/kata4/SQLTitleReader.java
+++ src/main/java/es/software/ulpgc/kata4/SQLTitleReader.java
@@ -1,6 +1,8 @@
 package es.software.ulpgc.kata4;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +11,21 @@ public class SQLTitleReader {
         List<Title> titles = new ArrayList<Title>();
         Connection con = SQLiteConnection.connect();
         if (con != null) {
-
+            String query = "SELECT * FROM Titles;";
+            try {
+                con.createStatement();
+                ResultSet rs = con.createStatement().executeQuery(query);
+                while(rs.next()) {
+                    String title = rs.getString("Title");
+                    String id = rs.getString("ID");
+                    String type = rs.getString("Type");
+                    Title t = new Title(id, Title.TitleType.valueOf(type), title);
+                    titles.add(t);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
+        return titles;
     }
 }
